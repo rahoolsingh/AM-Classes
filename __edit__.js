94119dbf9dc6601696522a575741395954f0508e
Rahul Kumar Singh <52322561+iamSinghRahul@users.noreply.github.com>
2021-02-13 16:33:21 +0530

    new

---
+++ __edit__.js
@@ -0,0 +1,36 @@
+// home page start here
+document.getElementById("notice").innerHTML=
+/*Sliding Line*/
+"Stay Safe, Eat Healthy Food, Boost Your Immunity | Live The New Normal"
+
+document.getElementById("blink").innerHTML=
+/*Blinking Text*/
+""
+
+document.getElementById("blink-link").innerHTML=
+/*Blinking Text Link*/
+""
+
+document.getElementById("high").innerHTML=
+/*Link Notice Board*/
+"Check New Schedule Here";
+
+document.getElementById("siteUpdate").innerHTML=
+/*Site Updated*/
+"Site Updated on: 28/01/2021";
+
+document.getElementById("apkVersion").innerHTML=
+/*APK Version*/
+"Version : 2.4 | Released on : 26 June 2020";
+
+
+// home page end here
+
+// Fee Structure Page
+/*
+To replace fee structure 
+1. go to replace folder
+2. replace "fee.jpg" with new fee structure
+
+use the same name and file extension
+*/
