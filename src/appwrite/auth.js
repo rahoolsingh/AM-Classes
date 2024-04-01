da07b1c60b7e2773a31fc1da5eb1b86bc4bb7f90
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-04-02 02:15:00 +0530

    Add AuthService class for authentication

---
+++ src/appwrite/auth.js
@@ -0,0 +1,51 @@
+import conf from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount(email, password, name) {
+        const userAccount = await this.account.create(
+            ID.unique(),
+            email,
+            password,
+            name
+        );
+
+        if (userAccount) {
+            // call the login method
+            return this.login(email, password);
+        } else {
+            return null;
+        }
+    }
+
+    async login(email, password) {
+        return await this.account.createEmailSession(email, password);
+    }
+
+    async getCurrentUser() {
+        return await this.account.get();
+    }
+
+    async logout() {
+        return await this.account.deleteSession("current");
+    }
+
+    async logoutAll() {
+        return await this.account.deleteSessions();
+    }
+}
+
+const authService = new AuthService();
+
+export { authService };
+export default AuthService;
